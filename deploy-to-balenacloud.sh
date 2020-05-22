83c9fd611e334801c3ba590e91121b06c76953e2
Matthew McGinn <matthew@balena.io>
2020-05-22 17:58:31 -0400

    add deploy-to-balenacloud.sh for balena app deploys
    HQ: https://github.com/balena-io/balena/pull/2177
    Change-type: patch
    Signed-off-by: Matthew McGinn <matthew@balena.io>

---
+++ deploy-to-balenacloud.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+BALENAOS_ACCOUNT="balena_os"
+
+echo "[INFO] Pulling ${TAG} from Docker Hub"
+docker pull "balena/${ARCH}-supervisor:${TAG}"
+echo "[INFO] Logging into ${API_ENDPOINT} as ${BALENAOS_ACCOUNT}"
+export BALENARC_BALENA_URL=${BALENARC_BALENA_URL}
+balena login --token "${API_TOKEN}"
+
+echo "[INFO] Pushing ${TAG} to ${BALENAOS_ACCOUNT}/${ARCH}-supervisor"
+_releaseID=$(balena deploy "${BALENAOS_ACCOUNT}/${ARCH}-supervisor" "balena/${ARCH}-supervisor:${TAG}" | sed -n 's/.*Release: //p')
+
+balena tag set version "${TAG}" --release "${_releaseID}"
